df06896bd392a09bc3cdffddf49e34f3a1e57265
pmatushkin <pmatushkin@gmail.com>
2015-06-20 16:37:17 -0400

    Finish Lesson 2

--- app/src/main/java/net/catsonmars/android/sunshine/app/MainActivity.java
+++ app/src/main/java/net/catsonmars/android/sunshine/app/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends ActionBarActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new MainActivityFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
