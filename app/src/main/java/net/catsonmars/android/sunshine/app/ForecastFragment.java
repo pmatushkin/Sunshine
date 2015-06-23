741dcd6900029032ced1dad4a34df6e0e21a4bc5
pmatushkin <pmatushkin@gmail.com>
2015-06-22 20:42:49 -0400

    Add toast when pressing on a forecast item

--- app/src/main/java/net/catsonmars/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/net/catsonmars/android/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package net.catsonmars.android.sunshine.app;
 
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,8 +13,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -96,6 +99,15 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String forecast = (String)parent.getAdapter().getItem(position);
+                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return rootView;
     }
